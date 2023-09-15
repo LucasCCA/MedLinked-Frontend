54e9b8f2c3dfbb20061a46a212ba7455aff53650
Lucas <lucasamarale@hotmail.com>
2023-09-15 18:29:28 -0300

    Correcoes ESLint, estilos globais e tema

---
+++ src/config/theme.ts
@@ -0,0 +1,31 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  border_radius: "4px",
+
+  colors: {
+    white: "rgba(255, 255, 255, 1.0)",
+    dark_blue_100: "rgba(0, 54, 134, 1.0)",
+    dark_blue_80: "rgba(0, 54, 134, 0.8)",
+    dark_blue_60: "rgba(0, 54, 134, 0.6)",
+    blue_100: "rgba(16, 67, 143, 1.0)",
+    blue_80: "rgba(16, 67, 143, 0.8)",
+    blue_60: "rgba(16, 67, 143, 0.6)",
+    light_blue_100: "rgba(81, 117, 171, 1.0)",
+    light_blue_80: "rgba(81, 117, 171, 0.8)",
+    light_blue_60: "rgba(81, 117, 171, 0.6)",
+    black_100: "rgba(33, 33, 33, 1.0)",
+    black_80: "rgba(33, 33, 33, 0.8)",
+    black_60: "rgba(33, 33, 33, 0.6)",
+    gray_100: "rgba(191, 191, 191, 1.0)",
+    gray_80: "rgba(191, 191, 191, 0.8)",
+    gray_60: "rgba(191, 191, 191, 0.6)",
+    red_100: "rgba(189, 0, 0, 1.0)",
+    red_80: "rgba(189, 0, 0, 0.8)",
+    red_60: "rgba(189, 0, 0, 0.6)",
+    yellow: "rgba(255, 223, 107, 1.0)",
+  },
+  fonts: {
+    roboto: "'Roboto', sans-serif",
+  },
+};
