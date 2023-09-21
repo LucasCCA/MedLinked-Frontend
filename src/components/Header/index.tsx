51621c957c289029bd5e8fc655a5af8af7cb1e93
Lucas <lucasamarale@hotmail.com>
2023-09-21 18:37:17 -0300

    Tentativa de Dropdown no Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,55 @@
-import { ChevronDown } from "lucide-react";
-import { Text } from "../Text";
-import { HeaderContainer, UserContainer } from "./styles";
+import { ChevronDown, LogOut, UserCircle2 } from "lucide-react";
+import { useState } from "react";
+import { Text } from "..";
+import {
+  ChevronContainer,
+  ClickableContainer,
+  HeaderContainer,
+  Option,
+  OptionsContainer,
+  UserContainer,
+} from "./styles";
 
 export function Header() {
+  const [openAnimation, setOpenAnimation] = useState(false);
+  const [closeAnimation, setCloseAnimation] = useState(false);
+
   return (
     <HeaderContainer>
       <UserContainer>
-        <Text color="white">Usuário</Text>
-        <ChevronDown />
+        <ClickableContainer
+          onClick={() => {
+            if (openAnimation) {
+              setOpenAnimation(false);
+              setCloseAnimation(true);
+            } else {
+              setOpenAnimation(true);
+              setCloseAnimation(false);
+            }
+          }}
+        >
+          <Text color="white">Usuário</Text>
+          <ChevronContainer
+            openAnimation={openAnimation}
+            closeAnimation={closeAnimation}
+          >
+            <ChevronDown />
+          </ChevronContainer>
+        </ClickableContainer>
+        {openAnimation && (
+          <OptionsContainer>
+            <div>
+              <Option>
+                <UserCircle2 />
+                <Text color="black_80">Perfil</Text>
+              </Option>
+              <Option>
+                <LogOut />
+                <Text color="black_80">Sair</Text>
+              </Option>
+            </div>
+          </OptionsContainer>
+        )}
       </UserContainer>
     </HeaderContainer>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,20 +1,116 @@
-import styled from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 
 export const HeaderContainer = styled.section`
   display: flex;
   justify-content: flex-end;
   align-items: center;
   background: ${(props) => props.theme.colors.dark_blue_100};
-  color: ${(props) => props.theme.colors.white};
   padding: 1.75rem 3.5rem;
 `;
 
 export const UserContainer = styled.div`
   display: flex;
+  justify-content: flex-start;
   align-items: center;
-  gap: 0.25rem;
+  color: ${(props) => props.theme.colors.black_80};
+
+  p {
+    padding-right: 0.25rem;
+  }
+`;
+
+export const ClickableContainer = styled.div`
+  display: flex;
+  align-items: center;
 
   :hover {
     cursor: pointer;
+  }
+`;
+
+const open = keyframes`
+  from {
+    transform: rotate(0);
+  }
+
+  to {
+    transform: rotate(180deg);
+  }
+`;
+
+const close = keyframes`
+  from {
+    transform: rotate(180deg);
+  }
+
+  to {
+    transform: rotate(0);
+  }
+`;
+
+export const ChevronContainer = styled.div<{
+  openAnimation: boolean;
+  closeAnimation: boolean;
+}>`
+  color: ${(props) => props.theme.colors.white};
+  ${({ openAnimation }) =>
+    openAnimation &&
+    css`
+      animation: ${open} 200ms;
+      animation-fill-mode: forwards;
+    `}
+
+  ${({ closeAnimation }) =>
+    closeAnimation &&
+    css`
+      animation: ${close} 200ms;
+      animation-fill-mode: forwards;
+    `}
+`;
+
+const height = keyframes`
+  from {
+    max-height: 0;
+  }
+
+  to {
+    max-height: 1000px;
+  }
+`;
+
+export const OptionsContainer = styled.div`
+  position: absolute;
+  margin-top: 100px;
+  width: 110px;
+  border-radius: ${(props) => props.theme.border_radius};
+  animation: ${height} 2s;
+
+  > div:first-child {
+    -moz-box-shadow: 0 0 6px ${(props) => props.theme.colors.black_60};
+    -webkit-box-shadow: 0 0 6px ${(props) => props.theme.colors.black_60};
+    box-shadow: 0 0 6px ${(props) => props.theme.colors.black_60};
+  }
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+export const Option = styled.div`
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid ${(props) => props.theme.colors.gray_80};
+  padding: 5px 1rem 5px 8px;
+  background: ${(props) => props.theme.colors.white};
+
+  p {
+    padding-left: 5px;
+  }
+
+  &:hover {
+    p,
+    svg {
+      color: ${(props) => props.theme.colors.black_100};
+    }
   }
 `;
