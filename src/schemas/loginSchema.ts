c8da05e29ff83be57e3b536ebe6400b35f5119cb
Lucas <lucasamarale@hotmail.com>
2023-10-12 22:29:33 -0300

    Tamanho minimo para senha e usuario

--- src/schemas/loginSchema.ts
+++ src/schemas/loginSchema.ts
@@ -5,11 +5,13 @@ export const loginSchema = yup
   .shape({
     username: yup
       .string()
-      .max(120, "O tamanho máximo do usuário é de 120 caracteres")
+      .min(3, "O tamanho mínimo do usuário deve ser de 3 caracteres")
+      .max(120, "O tamanho máximo do usuário deve ser de 120 caracteres")
       .required("O campo usuário é obrigatório"),
     password: yup
       .string()
-      .max(200, "O tamanho máximo da senha é de 120 caracteres")
+      .min(5, "O tamanho mínimo da senha deve ser de 5 caracteres")
+      .max(200, "O tamanho máximo da senha deve ser de 120 caracteres")
       .required("O campo senha é obrigatório"),
   })
   .required();
